a4ad580b3c99cbf56b29f3d86eb76db80b1db9d1
hamhem <ogskuoo@gmail.com>
2025-06-12 05:03:52 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,6 +45,8 @@ CURRENCIES = [
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+os.makedirs("/data", exist_ok=True)
+
 conn = sqlite3.connect("/data/users.db", check_same_thread=False)
 cursor = conn.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS balances (user_id INTEGER PRIMARY KEY, balance REAL)")
